e2b500ff29f8b93345f1461f88488ef57cc10850
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-02-12 22:14:11 -0300

    Integrando o banco de dados com o spring security

--- src/main/java/com/vinicius/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/com/vinicius/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,23 @@
 package com.vinicius.mvc.mudi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+
 public class WebSecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,20 +30,22 @@ public class WebSecurityConfig {
                         .permitAll()
                         .defaultSuccessUrl("/home")
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout((logout) -> logout
+                        .permitAll()
+                        .logoutUrl("/login"));
 
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("vini")
-                        .password("123")
-                        .roles("USER")
-                        .build();
 
-        return new InMemoryUserDetailsManager(user);
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder);
+
     }
+
 }
